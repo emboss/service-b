2bc19ca6c3b0c0f3a0e907cbbc37d4f17606aa68
Martin Boßlet <martin.bosslet@gmail.com>
2025-05-27 18:37:51 +0200

    feat: Add integration test

--- src/test/java/demo/serviceb/ServiceBApplicationTests.java
+++ src/test/java/demo/serviceb/ServiceBApplicationTests.java
@@ -1,13 +1,34 @@
 package demo.serviceb;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.ResponseEntity;
 
-@SpringBootTest
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ServicebApplicationTests {
+
+	@LocalServerPort
+	private int port;
+
+	@Autowired
+	private TestRestTemplate restTemplate;
 
 	@Test
 	void contextLoads() {
 	}
 
+	@Test
+	void getVersion_ShouldReturnVersionString() throws Exception {
+		ResponseEntity<String> response = restTemplate.getForEntity(
+			"http://localhost:" + port + "/api/version", String.class);
+		
+		assertThat(response.getStatusCode().value()).isEqualTo(200);
+		assertThat(response.getBody()).isNotNull();
+		assertThat(response.getBody()).isNotEmpty();
+	}
 }
